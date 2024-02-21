bc9a5048a3e8a81f150f62df3de19448121a9678
SianaStoicheva <ss12712978@edu1.pgzaimov.com>
2024-02-21 09:35:53 +0200

    new

--- ListEven/Program.cs
+++ ListEven/Program.cs
@@ -1,19 +1,43 @@
-﻿namespace ListEven
+﻿using System.Collections.Generic;
+
+namespace ListEven
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
-            List<int> listEven = new List<int>();
-            for (int i = 0; i < list.Count; i++)
-            {
-                if (list[i]%2 == 0)
-                {
-                    listEven.Add(list[i]);
-                }
-            }
-            Console.WriteLine(string.Join(" ",listEven));
+          //Otpecatvane na funkciite
+
+            Console.WriteLine("Chetni" + string.Join(" ",GetEvenElement(list)));
+            Console.WriteLine("Nechetni" + string.Join(" ",GetOddElement(list)));
         }
+        static List<int> GetEvenElement (List<int> intput) 
+        {
+            List<int> listEven = new List<int>();
+            for (int i = 0; i < intput.Count; i++)
+            {
+                if (intput[i] % 2 == 0)
+                {
+                    listEven.Add(intput[i]);
+                }
+               
+            }
+            return listEven;
+        } 
+        static List<int> GetOddElement (List<int> intput)
+        {
+            List<int> listOdd = new List<int>();
+            for (int i = 0; i < intput.Count; i++)
+            {
+                if (intput[i] % 2 != 0)
+                {
+                    listOdd.Add(intput[i]);
+                }
+
+            }
+            return listOdd;
+        }
+
     }
 }
